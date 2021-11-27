8438bfb493b73dbfd66ba2230cea71291e319767
iNikolas <realkolos1@gmail.com>
2021-11-27 21:41:24 +0200

    first commit

---
+++ confetti_cuisine_capstone_1/contentTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+module.exports = {
+    html: {
+        "Content-Type": "text/html"
+    },
+    text: {
+        "Content-Type": "text/plain"
+    },
+    css: {
+        "Content-Type": "text/css"
+    },
+    jpg: {
+        "Content-Type": "text/jpg"
+    },
+    png: {
+        "Content-Type": "text/png"
+    },
+    js: {
+        "Content-Type": "text/js"
+    }
+}
